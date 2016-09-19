922ab4c33877c70b3edfecbd6297597d5ad31b42
hyeon-woo <rlagusdn611@naver.com>
2016-09-19 17:06:53 +0900

    zz

---
+++ src/food/controller/maincontroller.java
@@ -0,0 +1,16 @@
+package food.controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by java on 2016-09-19.
+ */
+public class maincontroller implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
