23117c7b111f312253c667bca4206ae349c02852
hyeon-woo <rlagusdn611@naver.com>
2016-09-20 11:10:06 +0900

    회원가입 수정

--- src/food/controller/maincontroller.java
+++ src/food/controller/maincontroller.java
@@ -1,16 +1,47 @@
 package food.controller;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
+import java.awt.event.ActionEvent;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 /**
  * Created by java on 2016-09-19.
  */
-public class maincontroller implements Initializable {
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
+public class maincontroller{
+    @FXML private BorderPane menu1;
 
-    }
+        public void showMenu1(ActionEvent event)throws Exception{
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(
+                    "food/view/menu/menu1.fxml"
+            ));
+            try{
+                Parent root = loader.load();
+
+                Stage stage =(Stage)menu1.getScene().getWindow();
+                stage.setScene(new Scene(root));
+                stage.setTitle("foodmenu1");
+
+                stage.show();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+
+
+
+
+
+        }
+
+
+
+
 }
