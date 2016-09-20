7d12b8c7f2976e40decd16b7efeb80f39b051f42
byunghyunkim <byunghyun408@naver.com>
2016-09-20 12:31:17 +0900

    aa

--- src/kbh/sample/Main.java
+++ src/kbh/sample/Main.java
@@ -1,7 +1,31 @@
 package kbh.sample;
 
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /**
  * Created by java on 2016-09-20.
  */
-public class Main {
+public class Main extends Application{
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+   @Override
+    public void start(Stage stage) throws Exception {
+       FXMLLoader loader = new FXMLLoader(
+               getClass().getResource("managerlogin.fxml"));
+       Parent root = loader.load();
+
+       stage.setScene(new Scene(root));
+       stage.setResizable(false);
+
+       stage.show();
+
+   }
 }
+
